2616fcfcb9e5fa2449bc9849c56263b1dea84b12
Maciej <maciejbarel@gmail.com>
2017-08-22 11:46:55 +0200

    Remove unnecessary response var

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,21 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const ObjectID = require('mongodb').ObjectID;
-var mongoose = require('mongoose');
-var {Recipe} = require('./../models/recipe');
+let mongoose = require('mongoose');
+let {Recipe} = require('./../models/recipe');
 
 // Connect
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/RecipeMaster', {
   useMongoClient: true
 });
-
-// Response handling
-let response = {
-  status: 200,
-  data: [],
-  message: null
-};
 
 // Recipe API
 // Get recipes
