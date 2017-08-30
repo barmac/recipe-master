6cb513be16db3876353f0432e9ec30488edae71f
Maciej <maciejbarel@gmail.com>
2017-08-30 16:21:57 +0200

    Replace links in recipes view with (click) function

--- src/app/recipe/recipes/recipes.component.ts
+++ src/app/recipe/recipes/recipes.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import { Recipe } from '../recipe';
 import { RecipeService } from '../recipe.service';
+import {Router} from '@angular/router';
 
 
 @Component({
@@ -12,9 +13,14 @@ export class RecipesComponent implements OnInit {
   recipes: Recipe[];
 
   constructor(
-    public recipeService: RecipeService) { }
+    public recipeService: RecipeService,
+    public router: Router) { }
 
   ngOnInit(): void {
     this.recipeService.getRecipes().then(recipes => this.recipes = recipes);
+  }
+
+  goToRecipe(recipe: Recipe) {
+    this.router.navigate([`/recipe/${recipe._id}`]);
   }
 }
